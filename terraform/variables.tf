69b328682f3a777fec032b2cfbd784bbdd5db42a
Kolbe Face <48263839+kolbeface@users.noreply.github.com>
2023-08-08 11:16:08 -0700

    changed azs from east to west

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,7 +29,7 @@ variable "vpc" {
   default = {
     name                = "fleet"
     cidr                = "10.10.0.0/16"
-    azs                 = ["us-east-2a", "us-east-2b", "us-east-2c"]
+    azs                 = ["us-west-2a", "us-west-2b", "us-west-2c"]
     private_subnets     = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
     public_subnets      = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
     database_subnets    = ["10.10.21.0/24", "10.10.22.0/24", "10.10.23.0/24"]
