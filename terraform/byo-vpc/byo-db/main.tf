cb141849c40a1d5248e433cb0e2d162edf8b5629
Kolbe Face <48263839+kolbeface@users.noreply.github.com>
2023-08-28 16:26:31 -0700

    Update main.tf add aws_lb_listener resource

--- terraform/byo-vpc/byo-db/main.tf
+++ terraform/byo-vpc/byo-db/main.tf
@@ -80,8 +80,21 @@ module "alb" {
   ]
 }
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = "var.alb_config.certificate_arn"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
 resource "aws_lb_listener_rule" "static" {
-  listener_arn = aws_lb_listener.fleet.arn
+  listener_arn = aws_lb_listener.front_end.arn
   priority     = 1
 
   action {
